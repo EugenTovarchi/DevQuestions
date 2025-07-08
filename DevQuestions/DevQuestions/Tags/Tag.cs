34dae810a7d81f31b2381ecdef7aea7dda861130
Eugen T <rovanphone@gmail.com>
2025-07-08 13:58:20 +0300

    done domein models

---
+++ DevQuestions/DevQuestions/Tags/Tag.cs
@@ -0,0 +1,11 @@
+﻿namespace DevQuestions.Tags;
+
+public  class Tag
+{
+    public Guid Id { get; set; }
+
+    public string Descriptions { get; set; }
+
+    public required string Name { get; set; }
+
+}
